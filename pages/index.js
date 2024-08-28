9c013970db5f111723c109242c06e0a671174e1c
Sunday Ajiroghene <sunny.ajiroghene@gmail.com>
2024-08-28 14:00:52 +0100

    added clients components

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import About from "@/components/sections/About";
 import Award from "@/components/sections/Award";
 import Banner from "@/components/sections/Banner";
 import CallToAction from "@/components/sections/CallToAction";
+import Clients from "@/components/sections/Clients";
 import Contact from "@/components/sections/Contact";
 import Faq from "@/components/sections/Faq";
 import Features1 from "@/components/sections/Features1";
@@ -40,6 +41,7 @@ export default function Home2() {
         <Testimonial2 />
         <Award />
         <Features2T4 />
+        <Clients />
         <Contact />
         <News />
         <CallToAction />
