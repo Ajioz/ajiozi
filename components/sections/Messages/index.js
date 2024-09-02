abafceebe8b91264cd8e656d375bf8b6e2e8721d
Sunday Ajiroghene <sunny.ajiroghene@gmail.com>
2024-09-02 15:16:42 +0100

    implemented message list delete

--- components/sections/Messages/index.js
+++ components/sections/Messages/index.js
@@ -1,9 +1,14 @@
-import React from "react";
+import React, {useState} from "react";
 import { useRouter } from "next/router";
 import classes from "./messages.module.css";
+import { getAllEvents, getEventsById } from '@/dummy'
 
 const Messages = () => {
   const router = useRouter();
+
+  const [messages, setMessages] = useState(getAllEvents());
+
+  
   // Add this function at the top of your component or in a separate utils file
   const truncateMessage = (message, maxLength = 30) => {
     return message.length > maxLength
@@ -14,95 +19,44 @@ const Messages = () => {
   const handleDetails = (id) => {
     router.push(`/message/${id}`);
   };
-
+  
   const handleDelete = (id) => {
+    setMessages(getEventsById(messages, id));
     console.log(`${id} deleted!`);
   };
 
   return (
     <div className={classes.layout}>
-
-      <div className={classes.container} onClick={() => handleDetails(1)}>
-        <div className={classes.sn}>{truncateMessage(1, 3)}</div>
-        <div className={classes.name}>
-          {truncateMessage("Ajiroghene Sunday", 18)}
+      {messages.map((message, index) => (
+        <div
+          className={classes.container}
+          onClick={() => handleDetails(1)}
+          key={message.id}
+        >
+          <div className={classes.sn}>{truncateMessage(index+1, 3)}</div>
+          <div className={classes.name}>
+            {truncateMessage(message.name, 18)}
+          </div>
+          <div className={classes.message}>
+            {truncateMessage(message.message)}
+          </div>
+          <div className={classes.email}>
+            {truncateMessage(message.email, 20)}
+          </div>
+          <div className={classes.phone}>{truncateMessage(message.phone, 18)}</div>
+          <div className={classes.date}>
+            {truncateMessage(message.date, 14)}
+          </div>
+          <i
+            className={`${"icon fa fa-trash"} ${classes.chevron}`}
+            onClick={(e) => {
+              e.stopPropagation();
+              handleDelete(message.id);
+            }}
+          ></i>
+          <i className={`${"icon fa fa-chevron-right"} ${classes.chevron}`}></i>
         </div>
-        <div className={classes.message}>
-          {truncateMessage("I want to connect for a project")}
-        </div>
-        <div className={classes.email}>
-          {truncateMessage("sunny.ajiroghene@gmail.com", 20)}
-        </div>
-        <div className={classes.phone}>
-          {truncateMessage("+234 - 8064107055", 18)}
-        </div>
-        <div className={classes.date}>
-          {truncateMessage("Aug 24th, 2024", 14)}
-        </div>
-        <i
-          className={`${"icon fa fa-trash"} ${classes.chevron}`}
-          onClick={(e) => {
-            e.stopPropagation();
-            handleDelete(1);
-          }}
-        ></i>
-        <i className={`${"icon fa fa-chevron-right"} ${classes.chevron}`}></i>
-      </div>
-
-      <div className={classes.container} onClick={() => handleDetails(2)}>
-        <div className={classes.sn}>{truncateMessage(2, 3)}</div>
-        <div className={classes.name}>
-          {truncateMessage("Ajiroghene Sunday", 18)}
-        </div>
-        <div className={classes.message}>
-          {truncateMessage("I want to connect for a project")}
-        </div>
-        <div className={classes.email}>
-          {truncateMessage("sunny.ajiroghene@gmail.com", 20)}
-        </div>
-        <div className={classes.phone}>
-          {truncateMessage("+234 - 8064107055", 18)}
-        </div>
-        <div className={classes.date}>
-          {truncateMessage("Aug 24th, 2024", 14)}
-        </div>
-        <i
-          className={`${"icon fa fa-trash"} ${classes.chevron}`}
-          onClick={(e) => {
-            e.stopPropagation();
-            handleDelete(1);
-          }}
-        ></i>
-        <i className={`${"icon fa fa-chevron-right"} ${classes.chevron}`}></i>
-      </div>
-
-      <div className={classes.container} onClick={() => handleDetails(3)}>
-        <div className={classes.sn}>{truncateMessage(3, 3)}</div>
-        <div className={classes.name}>
-          {truncateMessage("Ajiroghene Sunday", 18)}
-        </div>
-        <div className={classes.message}>
-          {truncateMessage("I want to connect for a project")}
-        </div>
-        <div className={classes.email}>
-          {truncateMessage("sunny.ajiroghene@gmail.com", 20)}
-        </div>
-        <div className={classes.phone}>
-          {truncateMessage("+234 - 8064107055", 18)}
-        </div>
-        <div className={classes.date}>
-          {truncateMessage("Aug 24th, 2024", 14)}
-        </div>
-        <i
-          className={`${"icon fa fa-trash"} ${classes.chevron}`}
-          onClick={(e) => {
-            e.stopPropagation();
-            handleDelete(1);
-          }}
-        ></i>
-        <i className={`${"icon fa fa-chevron-right"} ${classes.chevron}`}></i>
-      </div>
-      
+      ))}
     </div>
   );
 };
--- dummy.js
+++ dummy.js
@@ -5,7 +5,7 @@ const DUMMY = [
     subject: "Programming for everyone",
     message:
       "Everyone can learn to code! Yes, everyone! In this live event, we are going to go through all the key basics and get you started with programming as well.",
-    email: "Some street 25, 12345 San Some where",
+    email: "emewasale211@gmail.com",
     phone: "+234-70968635686",
     date: "2021-05-12",
   },
@@ -15,7 +15,7 @@ const DUMMY = [
     subject: "Networking for introverts",
     message:
       "We know: Networking is no fun if you are an introvert person. That's why we came up with this event - it'll be so much easier. Promised!",
-    email: "New Wall Street 5, 98765 New Work",
+    email: "festus.onus@yahoo.com",
     phone: "+234-8326868668",
     date: "2021-05-30",
   },
@@ -25,7 +25,7 @@ const DUMMY = [
     subject: "Networking for extroverts",
     message:
       "You probably need no help with networking in general. But focusing your energy correctly - that is something where most people can improve.",
-    email: "My Street 12, 10115 Broke City",
+    email: "lovely_brian@hotmail.com",
     phone: "+234-6066368660",
     date: "2022-04-10",
   },
@@ -54,4 +54,8 @@ export function getFilteredEvents(dateFilter) {
 
 export function getEventById(id) {
   return DUMMY.find((event) => event.id === id);
+}
+
+export function getEventsById(arr, id) {
+  return arr.filter((event) => event.id !== id);
 }
