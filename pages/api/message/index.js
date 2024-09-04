a3c8471029d26a304193320c5d92d12abe3b538d
Sunday Ajiroghene <sunny.ajiroghene@gmail.com>
2024-09-04 10:09:32 +0100

    code refactoring

--- pages/api/message/index.js
+++ pages/api/message/index.js
@@ -1,4 +1,5 @@
 import { connectDB } from "@/utils/connectDB";
+import { ObjectId } from "mongodb"; // Added import for ObjectId
 
 export const insertDoc = async (client, collection, doc) => {
   const db = client.db();
@@ -23,139 +24,122 @@ const isValidEmail = (email) => {
 const messageHandler = async (req, res) => {
   let client;
 
-  if (req.method === "POST") {
-    try {
-      client = await connectDB();
-    } catch (error) {
-      console.error("Failed to connect to the database:", error);
-      return res.status(500).json({ message: "Database server error" });
-    }
+  try {
+    client = await connectDB();
 
-    try {
-      const { name, email, subject, phone, message } = req.body;
+    switch (req.method) {
+      case "POST":
+        try {
+          const { name, email, subject, phone, message } = req.body;
 
-      if (!isValidEmail(email)) {
-        return res
-          .status(422)
-          .json({ status: false, message: "Invalid email" });
-      }
+          if (!isValidEmail(email)) {
+            return res
+              .status(422)
+              .json({ status: false, message: "Invalid email" });
+          }
 
-      if (!name || name.trim() === "" || !message || message.trim() === "") {
-        return res
-          .status(422)
-          .json({ status: false, message: "Invalid input" });
-      }
+          if (
+            !name ||
+            name.trim() === "" ||
+            !message ||
+            message.trim() === ""
+          ) {
+            return res
+              .status(422)
+              .json({ status: false, message: "Invalid input" });
+          }
 
-      const newMessage = {
-        email,
-        name,
-        subject,
-        phone,
-        message,
-        isRead: false,
-      };
+          const newMessage = {
+            email,
+            name,
+            subject,
+            phone,
+            message,
+            isRead: false,
+          };
 
-      const result = await insertDoc(client, "message", newMessage);
-      newMessage.id = result._id;
+          const result = await insertDoc(client, "message", newMessage);
+          newMessage.id = result._id;
 
-      return res.status(201).json({
-        message: "Message saved successfully!",
-        newMessage,
-        status: true,
-      });
-    } catch (error) {
-      console.error("Error processing request:", error);
-      return res
-        .status(500)
-        .json({ message: "Failed to send message", status: false });
-    } finally {
-      client.close(); // Ensure the client is closed after the operations
-    }
-  } else if (req.method === "GET") {
-    let client;
-
-    try {
-      client = await connectDB();
-      const { id } = req.body;
-
-      if (id === undefined) {
-        const docs = getAllDocs(client, "message", { _id: -1 }, {});
-        return res.status(200).json({ docs });
-      } else {
-        const msg = await getOneDoc(client, "message", id);
-        if (!msg) {
+          return res.status(201).json({
+            message: "Message saved successfully!",
+            newMessage,
+            status: true,
+          });
+        } catch (error) {
+          console.error("Error processing request:", error);
           return res
-            .status(422)
-            .json({ message: `No message with id: ${id} exists` });
+            .status(500)
+            .json({ message: "Failed to send message", status: false });
         }
-        return res.status(200).json({ message: msg });
-      }
-    } catch (error) {
-      console.error("Error fetching messages:", error);
-      return res
-        .status(500)
-        .json({ message: "Error fetching messages", status: false });
-    } finally {
-      client.close(); // Ensure the client is closed after the operations
+
+      case "GET":
+        const { id } = req.body; // Keep using req.body
+        if (!id) {
+          const docs = await getAllDocs(client, "message", { _id: -1 }, {});
+          return res.status(200).json({ docs });
+        } else {
+          const msg = await getOneDoc(client, "message", id);
+          if (!msg) {
+            return res
+              .status(404)
+              .json({ message: `No message with id: ${id} exists` });
+          }
+          return res.status(200).json({ message: msg });
+        }
+
+      case "PATCH":
+        try {
+          const { id } = req.body;
+          const db = client.db();
+          const message = await db.collection("message").findOne({ _id: id });
+
+          if (!message) {
+            return res.status(404).json({ message: "No such message" });
+          }
+
+          const updateMsg = await db
+            .collection("message")
+            .updateOne({ _id: id }, { $set: { isRead: true } });
+
+          if (updateMsg) {
+            return res.status(201).json({ message: "Updated successfully" });
+          }
+        } catch (error) {
+          console.error("Error updating message:", error);
+          return res
+            .status(500)
+            .json({ message: "Error updating message", status: false });
+        }
+
+      case "DELETE":
+        const { id: deleteId } = req.body;
+        const objectId = ObjectId.createFromHexString(deleteId);
+        const deleteResult = await client
+          .db()
+          .collection("message")
+          .deleteOne({ _id: objectId });
+        if (deleteResult.deletedCount === 0) {
+          return res
+            .status(404)
+            .json({ message: "No such message or already deleted" });
+        }
+        return res
+          .status(200)
+          .json({ message: "Successfully deleted message" });
+
+      default:
+        return res
+          .status(405)
+          .json({ message: "Method not allowed", status: false });
     }
-  } else if (req.method === "PATCH") {
-    let client;
-
-    try {
-      client = await connectDB();
-      const { id } = req.body;
-      const db = client.db();
-      const message = await db.collection("message").findOne({ _id: id });
-
-      if (!message) {
-        return res.status(404).json({ message: "No such message" });
-      }
-
-      const updateMsg = await db
-        .collection("message")
-        .updateOne({ _id: id }, { $set: { isRead: true } });
-
-      if (updateMsg) {
-        return res.status(201).json({ message: "Updated successfully" });
-      }
-    } catch (error) {
-      console.error("Error updating message:", error);
-      return res
-        .status(500)
-        .json({ message: "Error updating message", status: false });
-    } finally {
-      client.close(); // Ensure the client is closed after the operations
-    }
-  }
-  
-  else if (req.method === "DELETE") {
-    let client;
-  
-    try {
-      client = await connectDB();
-      const { id } = req.body;
-      const deleteMsg = await client.db().collection("message").deleteOne({ _id: id });
-
-      if (!deleteMsg) {
-        return res.status(422).json({ message: "Could not delete message" });
-      }
-
-      return res.status(200).json({ message: "Successfully deleted message" });
-      
-    } catch (error) {
-      console.error("Error updating message:", error);
-      return res
-        .status(500)
-        .json({ message: "Error updating message", status: false });
-    } finally {
-      client.close(); // Ensure the client is closed after the operations
-    }
-  }
-  
-  else {
+  } catch (error) {
+    console.error(`Error in ${req.method} operation:`, error);
     return res
-      .status(405)
-      .json({ message: "Method not allowed", status: false });
+      .status(500)
+      .json({ message: `Error during ${req.method} operation`, status: false });
+  } finally {
+    if (client) client.close();
   }
 };
 
