808a43d72723ef8163a530095b63394d1237f3ca
Sunday Ajiroghene <sunny.ajiroghene@gmail.com>
2024-09-07 23:31:24 +0100

    cleanup

--- pages/contact.js
+++ pages/contact.js
@@ -2,9 +2,18 @@ import Layout from "@/components/layout/Layout";
 import PageTitle from "@/components/sections/PageTitle";
 import Contact from "@/components/sections/innerpages/Contact";
 export default function Pagecontact() {
+
+  const contactHead = {
+    headTitle: "Contact Ajiozi Services | Get in Touch with Our Team",
+    description:
+      "Reach out to Ajiozi Services for inquiries, support, or to discuss your project needs. Our team is here to assist you with professional software development, embedded systems, IoT, and tech solutions.",
+    keywords:
+      "Ajiozi contact, get in touch, software development inquiries, tech solutions support, embedded systems contact, IoT services, professional services contact, technology consulting, custom software inquiries",
+  };
+
   return (
     <>
-      <Layout headerStyle={1} footerStyle={1}>
+      <Layout headerStyle={1} footerStyle={1} head={contactHead}>
         <PageTitle pageName="Contact Us" />
         <Contact />
       </Layout>
--- pages/faq.js
+++ pages/faq.js
@@ -1,10 +1,19 @@
 import Layout from "@/components/layout/Layout";
 import PageTitle from "@/components/sections/PageTitle";
 import Faq from "@/components/sections/innerpages/Faq";
+
+  const faqHead = {
+    headTitle: "Frequently Asked Questions | Ajiozi Services",
+    description:
+      "Find answers to common questions about Ajiozi Services. Learn more about our software development, embedded systems, IoT solutions, and other tech services.",
+    keywords:
+      "Ajiozi FAQ, frequently asked questions, software development FAQ, tech solutions FAQ, embedded systems FAQ, IoT services FAQ, professional services FAQ, technology consulting FAQ, custom software FAQ",
+};
+  
 export default function pageFaq() {
   return (
     <>
-      <Layout headerStyle={1} footerStyle={1}>
+      <Layout headerStyle={1} footerStyle={1} head={faqHead}>
         <PageTitle pageName="Frequently Ask Questions" />
         <Faq />
       </Layout>
