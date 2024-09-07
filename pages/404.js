2460188534876f52db841b8f4f08fe7d87d82483
Sunday Ajiroghene <sunny.ajiroghene@gmail.com>
2024-09-07 23:23:46 +0100

    updated error page

--- pages/404.js
+++ pages/404.js
@@ -1,10 +1,22 @@
 import Layout from "@/components/layout/Layout";
-import Error from "@/components/sections/innerpages/Error";
+import dynamic from 'next/dynamic';
+
+const ErrorComponent = dynamic(() => import('@/components/sections/innerpages/Error'), {
+  ssr: false,
+});
+
 export default function PageError() {
+
+  const errorHead = {
+    headTitle: "404 Error | Page Not Found",
+    description: "The page you are looking for does not exist. Please check the URL or return to the homepage.",
+    keywords: "404 error, page not found, Ajiozi",
+  };
+
   return (
     <>
-      <Layout headerStyle={10} footerStyle={10}>
-        <Error />
+      <Layout headerStyle={10} footerStyle={10} head={errorHead}>
+        <ErrorComponent />
       </Layout>
     </>
   );
