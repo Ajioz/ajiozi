ef0635e0286c3be800c2ba84dd8c5c9b47c7ef26
Sunday Ajiroghene <sunny.ajiroghene@gmail.com>
2024-09-09 16:29:14 +0100

    added company address-map

--- components/sections/Map.js
+++ components/sections/Map.js
@@ -2,10 +2,18 @@
 
 export default function Map2() {
     return (
-        <>
+      <>
         <section className="map-section">
-            <iframe  className="map"  src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=1%20Grafton%20Street,%20Dublin,%20Ireland+(My%20Business%20Name)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe>
+   
+          <iframe
+            className="map"
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3971.1572247592753!2d5.766990774075362!3d5.543691533775216!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1041ad867f385ec3%3A0xa23612766ebeb996!2sAjiozi%20Limited!5e0!3m2!1sen!2sng!4v1725893611743!5m2!1sen!2sng"
+            style={{ width: "100%", height: "600px", border: 0 }}
+            allowFullScreen=""
+            loading="lazy"
+            referrerPolicy="no-referrer-when-downgrade"
+          ></iframe>
         </section>
-        </>
-    )
+      </>
+    );
 }
