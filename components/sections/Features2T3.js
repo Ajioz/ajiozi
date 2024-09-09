2bac520cad5455e369e5d966ae99e6f23b58c56e
Sunday Ajiroghene <sunny.ajiroghene@gmail.com>
2024-09-09 12:40:29 +0100

    added industrial view

--- components/sections/Features2T3.js
+++ components/sections/Features2T3.js
@@ -2,49 +2,68 @@ const Features2T3 = () => {
   return (
     <>
       <section className="features-section-five pt-0 pb-0">
-        <div className="row g-0"> 
+        <div className="row g-0">
           <div className="image-column col-xl-4">
-            <figure className="image"><img src="images/resource/feature4-1.jpg" alt="Image"/></figure>
+            <figure className="image">
+              <img src="images/resource/feature4-1.jpg" alt="Image" />
+            </figure>
           </div>
           <div className="content-column col-xl-4 col-lg-6">
             <div className="inner-column">
               <div className="icon-arrow3 bounce-x"></div>
-              <div className="sec-title dark"> <span className="sub-title">What we Offer</span>
-                <h2>The Best Source for <br/>IT Solutions</h2>
+              <div className="sec-title dark">
+                {" "}
+                <span className="sub-title">Our Industrial Landscape</span>
+                <h2>
+                  Leading Solutions for <br />
+                  Industries
+                </h2>
               </div>
             </div>
           </div>
           <div className="icons-column col-xl-4 col-lg-6">
             <div className="inner-column">
-              <div className="row g-0"> 
+              <div className="row g-0">
                 <div className="feature-block-five col-md-4 col-sm-6">
-                  <div className="inner"> <i className="icon flaticon-digital-services"></i>
-                    <h5 className="title">Website</h5>
+                  <div className="inner">
+                    {" "}
+                    <i className="icon fa fa-city"></i>
+                    <h5 className="title">Automation</h5>
                   </div>
                 </div>
                 <div className="feature-block-five col-md-4 col-sm-6">
-                  <div className="inner"> <i className="icon flaticon-targeted-marketing"></i>
-                    <h5 className="title">Ios</h5>
+                  <div className="inner">
+                    {" "}
+                    <i className="icon fa fa-robot"></i>
+                    <h5 className="title">Robotics</h5>
                   </div>
                 </div>
                 <div className="feature-block-five col-md-4 col-sm-6">
-                  <div className="inner"> <i className="icon flaticon-promotion"></i>
-                    <h5 className="title">Android</h5>
+                  <div className="inner">
+                    {" "}
+                    <i className="icon fa fa-atom"></i>
+                    <h5 className="title">Smart Energy</h5>
                   </div>
                 </div>
                 <div className="feature-block-five col-md-4 col-sm-6">
-                  <div className="inner"> <i className="icon flaticon-graphic-design"></i>
+                  <div className="inner">
+                    {" "}
+                    <i className="icon flaticon-graphic-design"></i>
                     <h5 className="title">Iot</h5>
                   </div>
                 </div>
                 <div className="feature-block-five col-md-4 col-sm-6">
-                  <div className="inner"> <i className="icon flaticon-laptop"></i>
+                  <div className="inner">
+                    {" "}
+                    <i className="icon fas fa-watch"></i>
                     <h5 className="title">Wearable</h5>
                   </div>
                 </div>
                 <div className="feature-block-five col-md-4 col-sm-6">
-                  <div className="inner"> <i className="icon flaticon-teaching"></i>
-                    <h5 className="title">Tv</h5>
+                  <div className="inner">
+                    {" "}
+                    <i className="icon fa fa-tools"></i>
+                    <h5 className="title">Maintenance</h5>
                   </div>
                 </div>
               </div>
