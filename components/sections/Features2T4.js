c60e2a8a82db52238face6972bf642faf1a404bd
Sunday Ajiroghene <sunny.ajiroghene@gmail.com>
2024-09-09 14:20:22 +0100

    added curltural heritage

--- components/sections/Features2T4.js
+++ components/sections/Features2T4.js
@@ -1,45 +1,67 @@
 const Features2T4 = () => {
   return (
     <>
-        <section className="features-section-six">
-          <div className="bg bg-pattern-7"></div>
-          <div className="auto-container">
-            <div className="outer-box">
-              <div className="row"> 
-                <div className="content-column col-xl-5 col-lg-6">
-                  <div className="inner-column wow fadeInLeft">
-                    <div className="sec-title"> <span className="sub-title">Company Features</span>
-                      <h2>Grow business with endless possibility</h2>
+      <section className="features-section-six">
+        <div className="bg bg-pattern-7"></div>
+        <div className="auto-container">
+          <div className="outer-box">
+            <div className="row">
+              <div className="content-column col-xl-5 col-lg-6">
+                <div className="inner-column wow fadeInLeft">
+                  <div className="sec-title">
+                    {" "}
+                    <span className="sub-title">Our Culture</span>
+                    <h2>Empowering Growth with Innovation</h2>
+                  </div>
+                  <div className="info-box">
+                    {" "}
+                    <span className="count">01</span>
+                    <h4 className="title">Inclusive and Collaborative</h4>
+                    <div className="text">
+                      We foster a culture of inclusivity and collaboration,
+                      ensuring every voice is heard and valued.
                     </div>
-                    <div className="info-box"> <span className="count">01</span>
-                      <h4 className="title">No coding skills required</h4>
-                      <div className="text">There are many variations of simply free text passages of available but the majority</div>
+                  </div>
+                  <div className="info-box">
+                    {" "}
+                    <span className="count">02</span>
+                    <h4 className="title">Continuous Learning</h4>
+                    <div className="text">
+                      Our commitment to continuous learning drives innovation
+                      and excellence in everything we do.
                     </div>
-                    <div className="info-box"> <span className="count">02</span>
-                      <h4 className="title">Great documentation</h4>
-                      <div className="text">There are many variations of simply free text passages of available but the majority</div>
-                    </div>
-                    <div className="info-box"> <span className="count">03</span>
-                      <h4 className="title">SEO optimized</h4>
-                      <div className="text">There are many variations of simply free text passages of available but the majority</div>
+                  </div>
+                  <div className="info-box">
+                    {" "}
+                    <span className="count">03</span>
+                    <h4 className="title">Customer-Centric Approach</h4>
+                    <div className="text">
+                      We prioritize our customers' needs, delivering solutions
+                      that exceed expectations and create lasting value.
                     </div>
                   </div>
                 </div>
-                <div className="image-column col-xl-7 col-lg-6">
-                  <div className="inner-column wow fadeInRight" data-wow-delay="300ms">
-                    <div className="image-box">
-                      <figure className="image"><img src="images/resource/feature6-1.jpg" alt="Image"/></figure>
-                      <div className="caption-box">
-                        <h4 className="caption">We are standout experts <br/>in digital agency</h4>
-                        <div className="icon-arrow4"></div>
-                      </div>
+              </div>
+              <div className="image-column col-xl-7 col-lg-6">
+                <div
+                  className="inner-column wow fadeInRight"
+                  data-wow-delay="300ms"
+                >
+                  <div className="image-box">
+                    <figure className="image">
+                      <img src="images/resource/feature6-1.jpg" alt="Image" />
+                    </figure>
+                    <div className="caption-box">
+                      <h4 className="caption">Rich in cultural heritage</h4>
+                      <div className="icon-arrow4"></div>
                     </div>
                   </div>
                 </div>
               </div>
             </div>
           </div>
-        </section>
+        </div>
+      </section>
     </>
   );
 };
