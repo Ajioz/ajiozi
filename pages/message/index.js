5da8d4cc4d60e94ac7480261ca918f9c0a2e8a1d
Sunday Ajiroghene <sunny.ajiroghene@gmail.com>
2024-09-15 11:28:36 +0100

    undo changes

--- pages/message/index.js
+++ pages/message/index.js
@@ -1,33 +1,20 @@
-// export async function getServerSideProps(context) {
-//   const session = await getSession({ req: context.req });
-//   console.log("Session:", session); // Debugging to check session structure
+import Messages from "@/components/sections/Messages";
+import Layout from "@/components/layout/Layout";
+import PageTitle from "@/components/sections/PageTitle";
+import { fetchMessages } from "@/utils/util-fetch";
 
-//   let messages = [];
+const MessagesPage = ({ messages }) => {
+  return (
+    <>
+      <Layout headerStyle={1} footerStyle={1}>
+        <PageTitle pageName="Messages" />
+        <Messages messages={messages} />
+      </Layout>
+    </>
+  );
+};
 
-//   try {
-//     messages = await fetchMessages();
-//   } catch (error) {
-//     console.error("Error fetching messages:", error);
-//     messages = [];
-//   }
-
-//   // Check if session exists and has a valid user
-//   if (!session || !session.user) {
-//     return {
-//       redirect: {
-//         destination: "/",
-//         permanent: false,
-//       },
-//     };
-//   }
-
-//   return {
-//     props: {
-//       session,
-//       messages,
-//     },
-//   };
-// }
+export default MessagesPage;
 
 
 export async function getServerSideProps(context) {
@@ -60,3 +47,35 @@ export async function getServerSideProps(context) {
     },
   };
 }
+
+
+// export async function getServerSideProps(context) {
+//   const session = await getSession({ req: context.req });
+//   console.log("Session:", session); // Debugging to check session structure
+
+//   let messages = [];
+
+//   try {
+//     messages = await fetchMessages();
+//   } catch (error) {
+//     console.error("Error fetching messages:", error);
+//     messages = [];
+//   }
+
+//   // Check if session exists and has a valid user
+//   if (!session || !session.user) {
+//     return {
+//       redirect: {
+//         destination: "/",
+//         permanent: false,
+//       },
+//     };
+//   }
+
+//   return {
+//     props: {
+//       session,
+//       messages,
+//     },
+//   };
+// }
