6e2acdb1981b146569c83bea22e2e10fdd134671
Sunday Ajiroghene <sunny.ajiroghene@gmail.com>
2024-09-15 01:03:39 +0100

    improve message

--- pages/message/index.js
+++ pages/message/index.js
@@ -1,36 +1,50 @@
-import React from "react";
-import { getSession } from "next-auth/react";
-import Messages from "@/components/sections/Messages";
-import Layout from "@/components/layout/Layout";
-import PageTitle from "@/components/sections/PageTitle";
-import { fetchMessages } from "@/utils/util-fetch";
+// export async function getServerSideProps(context) {
+//   const session = await getSession({ req: context.req });
+//   console.log("Session:", session); // Debugging to check session structure
 
-const MessagesPage = ({ messages }) => {
-  return (
-    <>
-      <Layout headerStyle={1} footerStyle={1}>
-        <PageTitle pageName="Messages" />
-        <Messages messages={messages} />
-      </Layout>
-    </>
-  );
-};
+//   let messages = [];
 
-export default MessagesPage;
+//   try {
+//     messages = await fetchMessages();
+//   } catch (error) {
+//     console.error("Error fetching messages:", error);
+//     messages = [];
+//   }
+
+//   // Check if session exists and has a valid user
+//   if (!session || !session.user) {
+//     return {
+//       redirect: {
+//         destination: "/",
+//         permanent: false,
+//       },
+//     };
+//   }
+
+//   return {
+//     props: {
+//       session,
+//       messages,
+//     },
+//   };
+// }
+
 
 export async function getServerSideProps(context) {
   const session = await getSession({ req: context.req });
+  console.log("Session:", session); // Debugging to check session structure
+
   let messages = [];
 
   try {
     messages = await fetchMessages();
   } catch (error) {
     console.error("Error fetching messages:", error);
-    // Handle the error appropriately, e.g., set messages to an empty array or a default value
-    messages = []; // or you can set a default message
+    messages = [];
   }
 
-  if (!session) {
+  // Check if session exists and has a valid user
+  if (!session || !session.user) {
     return {
       redirect: {
         destination: "/",
@@ -38,6 +52,7 @@ export async function getServerSideProps(context) {
       },
     };
   }
+
   return {
     props: {
       session,
