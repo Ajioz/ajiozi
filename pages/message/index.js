a9f220ed211c511952d0067a5a8e47a0d2866cc3
Sunday Ajiroghene <sunny.ajiroghene@gmail.com>
2024-09-15 11:49:57 +0100

    little cleanup

--- pages/message/index.js
+++ pages/message/index.js
@@ -48,35 +48,3 @@ export async function getServerSideProps(context) {
     },
   };
 }
-
-
-// export async function getServerSideProps(context) {
-//   const session = await getSession({ req: context.req });
-//   console.log("Session:", session); // Debugging to check session structure
-
-//   let messages = [];
-
-//   try {
-//     messages = await fetchMessages();
-//   } catch (error) {
-//     console.error("Error fetching messages:", error);
-//     messages = [];
-//   }
-
-//   // Check if session exists and has a valid user
-//   if (!session || !session.user) {
-//     return {
-//       redirect: {
-//         destination: "/",
-//         permanent: false,
-//       },
-//     };
-//   }
-
-//   return {
-//     props: {
-//       session,
-//       messages,
-//     },
-//   };
-// }
