af47285488182d15d6c33149f870e1dfcd2f65a1
Ajioz <sunny.ajiroghene@gmail.com>
2024-11-01 11:32:27 +0100

    preparing or blog

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,3 +1,5 @@
+// Production
+/*
 import NextAuth from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 import { verifyPassword } from "@/utils/auth";
@@ -51,3 +53,57 @@ export default NextAuth({
     }),
   ],
 });
+*/
+
+
+// Development
+// /*
+import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import { connectDB } from "../../../utils/connectDB";
+import { verifyPassword } from "../../../utils/auth";
+
+export default NextAuth({
+  session: {
+    strategy: "jwt",
+  },
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        token.id = user.id;
+        token.email = user.email;
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      if (token) {
+        session.user.id = token.id;
+        session.user.email = token.email;
+      }
+      return session;
+    },
+  },
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      async authorize(credentials) {
+        const client = await connectDB();
+        const usersCollection = client.db().collection("admin");
+        const user = await usersCollection.findOne({
+          email: credentials.email,
+        });
+
+        if (!user) throw new Error("No user found");
+        const isValidPassword = await verifyPassword(
+          credentials.password,
+          user.password
+        );
+
+        if (!isValidPassword) throw new Error("Invalid password");
+        return { email: user.email, id: user._id.toString() };
+      },
+    }),
+  ],
+  debug: true,
+});
+// */
--- utils/connectDB.js
+++ utils/connectDB.js
@@ -1,9 +1,9 @@
 import { MongoClient } from "mongodb";
 import { notFound } from "next/navigation";
 
-// const connectString = process.env.MONGODB_URL_LOCAL;
+const connectString = process.env.MONGODB_URL_LOCAL;
 // const connectString = process.env.MONGODB_URL_DEV;
-const connectString = process.env.MONGODB_URL_PROD;
+// const connectString = process.env.MONGODB_URL_PROD;
 
 // const connectString = `mongodb+srv://${process.env.mongodb_username}:${process.env.mongodb_password}@${process.env.mongodb_cluster}.wu3eq.mongodb.net/${mongodb_database}?retryWrites=true&w=majority`;
 
