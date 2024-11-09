3d48b98d97125b81255a4a43b8d59659214305b7
Ajioz <sunny.ajiroghene@gmail.com>
2024-11-09 20:38:08 +0100

    fixed code block shade

--- utils/connectDB.js
+++ utils/connectDB.js
@@ -1,8 +1,8 @@
 import { MongoClient } from "mongodb";
 
-const connectString = process.env.MONGODB_URL_LOCAL;
+// const connectString = process.env.MONGODB_URL_LOCAL;
 // const connectString = process.env.MONGODB_URL_DEV;
-// const connectString = process.env.MONGODB_URL_PROD;
+const connectString = process.env.MONGODB_URL_PROD;
 
 // const connectString = `mongodb+srv://${process.env.mongodb_username}:${process.env.mongodb_password}@${process.env.mongodb_cluster}.wu3eq.mongodb.net/${mongodb_database}?retryWrites=true&w=majority`;
 
